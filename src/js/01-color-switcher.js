7d1b1432a7737114b985657a1a813fa936405ba1
YarynaLe <109382584+YarynaLe@users.noreply.github.com>
2023-01-18 22:21:11 +0200

    +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,11 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
